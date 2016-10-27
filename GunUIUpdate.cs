e9bb89370334480bf2a090cfdee57140e81c51a2
vasujogani <vasujogani7@gmail.com>
2016-10-27 14:24:19 -0500

    Add files via upload

---
+++ GunUIUpdate.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GunUIUpdate : MonoBehaviour {
+
+	string currentGunName = "";
+	public Text text;
+
+	// Use this for initialization
+	void Start () {
+		text.text = "";
+	}
+	
+	// Update is called once per frame
+	public void ReloadUpdate(){
+		text.text = "Reloading... Please Wait!";
+	}
+
+	public void UpdateUI(int b, string g){
+		text.text = "Gun: " + g + "\n Bullets: " + b;
+	}
+}
